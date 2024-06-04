d53e6fd422e7f35fd4797d13940f5b337420ad9f
AngeloSouza1 <angeloafdesouza@gmail.com>
2024-06-04 13:21:18 -0300

    feat: criando login usuario

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-  root "home#index"
+    devise_for :admins
+
+    authenticated :admin do
+      root to: "admin#index", as: :admin_root
+    end
+    get "admin" => "admin#index"
+    root "home#index"
+
 end
