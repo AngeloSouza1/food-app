561306ab3714411d88f748204ff16129ec189d80
AngeloSouza1 <angeloafdesouza@gmail.com>
2024-06-05 13:50:40 -0300

    feat: gerecimaneto de estoque

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,17 @@
 Rails.application.routes.draw do
   namespace :admin do
-    resources :products
+    resources :products do
+      resources :stocks
+    end
+
     resources :categories
   end
-    devise_for :admins
+  devise_for :admins
+  root "home#index"
 
-    authenticated :admin do
-      root to: "admin#index", as: :admin_root
-    end
-    get "admin" => "admin#index"
-    root "home#index"
+  authenticated :admin_user do
+    root to: "admin#index", as: :admin_root
+  end
 
+  get "admin" => "admin#index"
 end
